2093b927cf7d27ab8f1542853059b924668c9ba0
Asheesh Sharma <144527342+asheeshkumar933@users.noreply.github.com>
2024-08-09 16:38:19 +0000

    color changer project

---
+++ JavaScriptlearinin/Datatype.js
@@ -0,0 +1,30 @@
+ var UserName = " Asheesh sharma " // String type 
+ var UserId  = 1212121 // Number 
+ var IsLoggedIn = true // Boolean 
+var UserCity = null  // Null is a stdndalon value 
+ var UserAccount ; // Undefined 
+
+
+/* 
+-->  variable are case sensitive .
+--> Only letter, Digits , underScore and doller sign are allow (space is not allow ) , varriable first letter 
+--> Reserved word are not allow in varriable.
+
+
+var varriable are not used because of block scope problem ,
+const and let are used in this language . 
+
+
+Premitive data type -- 
+String , Number , Null , Undifind , Symbol , Boolean ,BigInt 
+
+
+Refrence type (non- premitive )
+
+Array , Object , Function 
+ 
+
+*/
+const Id = Symbol('12121')
+const anotherId = Symbol("5454")
+console.log(Id === anotherId) 
